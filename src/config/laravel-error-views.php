b4c8bfdd9fa8d0779c9b5f1b6a1fcc1f026718d2
Ridwan Kasim <kasimsheyi20@gmail.com>
2024-07-21 12:16:24 +0100

    minor fixes

--- src/config/laravel-error-views.php
+++ src/config/laravel-error-views.php
@@ -7,15 +7,14 @@ return [
     | Default Page Title
     |--------------------------------------------------------------------------
     |
-    | Here you may specify the title that will be displayed on all error pages
+    | Here you may specify the title that will be displayed for each of the error pages
     |
     */
-    'titles' => [
-        '404' => env('404_TITLE', 'Page Not Found!'),
-        '500' => env('500_TITLE', 'Server Error!'),
-        '403' => env('403_TITLE', 'Access Denied!'),
-        '401' => env('401_TITLE', 'Unauthorized!'),
-        '503' => env('503_TITLE', 'Service Unavailable!')
+    'title' => [
+        '403' => env('LARAVEL_ERROR_VIEWS_403_TITLE', 'Action or Page not authorized!!!'),
+        '404' => env('LARAVEL_ERROR_VIEWS_404_TITLE', 'Page Not Found!'),
+        '500' => env('LARAVEL_ERROR_VIEWS_500_TITLE', 'Whoops!!! Something went wrong.'),
+        '503' => env('LARAVEL_ERROR_VIEWS_503_TITLE', 'Whoops!!! Service is currently unavailable')
     ],
 
     /*
@@ -23,15 +22,33 @@ return [
     | Default Messages
     |--------------------------------------------------------------------------
     |
-    | Here you may specify the message that will be displayed on all error pages
+    | Here you may specify the message that will be displayed for each of the error pages
     |
     */
-    'messages' => [
-        '404' => env('404_MESSAGE', 'Page Not Found'),
-        '500' => env('500_MESSAGE', 'Server Error'),
-        '403' => env('403_MESSAGE', 'Access Denied'),
-        '401' => env('401_MESSAGE', 'Unauthorized'),
-        '503' => env('503_MESSAGE', 'Service Unavailable')
+    'message' => [
+        '403' => env('LARAVEL_ERROR_VIEWS_403_MESSAGE', 'Sorry, You do not have access to this page or resource.'),
+        '404' => env('LARAVEL_ERROR_VIEWS_404_MESSAGE', 'It seems the page or resource you are looking for doesn\'t exist or has been moved.'),
+        '500' => env('LARAVEL_ERROR_VIEWS_500_MESSAGE', 'Whoops!!! It\'s not you, it\'s us. Please try again.'),
+        '503' => env('LARAVEL_ERROR_VIEWS_503_MESSAGE', 'Sorry, we are doing some maintenance. Please try again in few minutes.')
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Customizing the colors for both the title, message and, back button
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the text colors for both the title & message
+    |
+    */
+    'colors' => [
+        'text' => [
+            'title' => env('LARAVEL_ERROR_VIEWS_COLORS_TITLE', 'text-gray-700'),
+            'message' => env('LARAVEL_ERROR_VIEWS_COLORS_MESSAGE', 'text-gray-500')
+        ],
+        'button' => [
+            'background' => env('LARAVEL_ERROR_VIEWS_COLORS_BUTTON_BACKGROUND', 'bg-purple-600'),
+            'text' => env('LARAVEL_ERROR_VIEWS_COLORS_BUTTON_TEXT', 'text-purple-600')
+        ]
     ],
 
 ];
--- src/views/errors/layout.blade.php
+++ src/views/errors/layout.blade.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=typography,aspect-ratio"></script>
 </head>
+
 <body class="bg-white">
-    <div class="container">
+    <div class="container mx-auto">
         <div class="grid h-screen place-items-center mx-4">
             <div class="text-center">
                 @yield('logo')
                 <div class="space-y-5">
-                    <h2 class="font-custom-medium text-xl">@yield('title')</h2>
-                    <p class="font-custom-light text-gray-500">@yield('message')</p>
+                    <h2 class="font-semibold text-xl {{ config('laravel-error-views.colors.text.title') }}">
+                        @yield('title')</h2>
+
+                    <p class="font-normal {{ config('laravel-error-views.colors.text.message') }}">@yield('message')</p>
+
                     <div class="flex justify-center">
-                        <a href="{{ URL::previous() }}" class="text-purple-one flex items-center space-x-1 font-custom-semibold">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
+                        <a href="{{ URL::previous() }}"
+                            class="{{ config('laravel-error-views.colors.button.text') }} flex items-center space-x-1 font-semibold">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                             </svg>
@@ -32,4 +38,5 @@
         </div>
     </div>
 </body>
+
 </html>
